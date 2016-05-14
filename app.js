c94b08873ae488aa86f8c4eb648f306e901ab113
K. Kulikov <rasmadeus@gmail.com>
2016-05-14 16:02:53 +0300

    Migrate from static to serve favicon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 nunjucks.setup({autoescape: true, throwOnUndefined: false, trimBlocks: false, lstripBlocks: false, watch: true, noCache: true, tags: {}}, app);
 
-app.use(favicon());
+app.use(favicon(path.join(__dirname, '/public/img/favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
