2d0c9b355cc564646097ae213484be43c664aa16
K. Kulikov <rasmadeus@gmail.com>
2016-05-14 21:09:35 +0300

    Added login template

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res) {
+    res.render('login', { title: 'авторизация' });
 });
 
 module.exports = router;
