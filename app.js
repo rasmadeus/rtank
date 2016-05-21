0b7f5344fe5d6b75bc85ccc5139a0c8fecdee552
K. Kulikov <rasmadeus@gmail.com>
2016-05-21 21:18:45 +0300

    Refactoring: split big application to little functions

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+function setup_logger(app) {
+    var logger = require('morgan');
+    app.use(logger('dev'));
+}
+
 function setup_view(app) {
     var nunjucks = require('express-nunjucks');
-    app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'html');
     nunjucks.setup(
         {
@@ -16,36 +20,56 @@ function setup_view(app) {
     );
 }
 
-function setup_static_paths(app) {
+function setup_static_paths(app, express) {
+    var path = require('path');
+
+    app.set('views', path.join(__dirname, 'views'));
+
+    var favicon = require('serve-favicon');
+    app.use(favicon(path.join(__dirname, '/public/img/favicon.png')));
+
     app.use(express.static(path.join(__dirname, 'public')));
     app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
     app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
     app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 }
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+function setup_session(app) {
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded());
 
+    var cookieParser = require('cookie-parser');
+    app.use(cookieParser());
 
-var app = express();
-var passport = require('./apps/user/passport')(app);
-setup_view(app);
+    var session = require('express-session');
+    app.use(session({ secret: 'this is a tankappliocationsuper game by author rasmadeus', resave: false, saveUninitialized: false }));
+}
 
-app.use(favicon(path.join(__dirname, '/public/img/favicon.png')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-setup_static_paths(app);
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use('/', require('./apps/root/index')());
-app.use('/users', require('./apps/user/urls')(passport));
-app.use(require('./apps/root/error'));
+function setup_routing(app) {
+    var root = require('./apps/root/urls');
+    app.use('/', root());
 
-module.exports = app;
+    var users = require('./apps/user/urls');
+    var passport = require('./apps/user/passport')(app);
+    app.use('/users', users(passport));
+
+    var error = require('./apps/root/error');
+    app.use(error);
+}
+
+function make_application() {
+    var express = require('express');
+
+    var app = express();
+    setup_logger(app);
+    setup_view(app);
+    setup_static_paths(app, express);
+    setup_session(app);
+    setup_routing(app);
+
+    return app;
+}
+
+module.exports = make_application();
 
