05f39bdb3e15d6f1641b591c1e63bb5c2f1ae724
K. Kulikov <rasmadeus@gmail.com>
2016-05-28 20:50:52 +0300

    -Add flash message

--- apps/user/views.js
+++ apps/user/views.js
@@ -11,8 +11,42 @@ function signup(req, res) {
     res.render('signup', {title: 'registration'});
 }
 
+function create_user(req, res, User) {
+    var user = new User();
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.save(function (er){
+        if (er) {
+            req.flash('error', er);
+            signup(req, res);
+        }
+        else {
+            res.redirect('/');
+        }
+    });
+}
+
+function register_user(req, res) {
+    var User = require('./models').User;
+    User.findOne({'email':  req.body.email}, function(er, user) {
+        if (er) {
+            req.flash('error', er);
+            signup(req, res);
+        }
+        else if (user) {
+            req.flash('error', 'User "' + req.body.email + '" already exist.');
+            signup(req, res);
+        }
+        else
+        {
+            create_user(req, res, User);
+        }
+    });
+}
+
 module.exports = {
     login: login,
     logout: logout,
-    signup: signup
+    signup: signup,
+    register_user: register_user
 };
